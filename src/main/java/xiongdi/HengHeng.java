98221ad4b1fdc141eb48c7d7ffef3212fbd00b5e
Gelai <2267643286@qq.com>
2022-03-09 21:19:53 +0800

    second zz

--- src/main/java/xiongdi/HengHeng.java
+++ src/main/java/xiongdi/HengHeng.java
@@ -3,5 +3,8 @@ package xiongdi;
 public class HengHeng {
     public static void main(String[] args) {
         System.out.println("hehe");
+        System.out.println("hehe");
+        System.out.println("hehe");
+
     }
 }
