bcfe61e7907954473e586df8a63f0f206143b3f1
Gelai <2267643286@qq.com>
2022-03-10 10:30:31 +0800

    hot1

--- src/main/java/xiongdi/HengHeng.java
+++ src/main/java/xiongdi/HengHeng.java
@@ -10,5 +10,6 @@ public class HengHeng {
         System.out.println("hothothot");
         System.out.println("就是试试试试试试！");
         System.out.println("不明白有什么可试试的！");
+        System.out.println("用马云试试呗");
     }
 }
