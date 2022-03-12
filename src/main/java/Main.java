debef463b66704b63553e636c21619ba33e5883c
Gelai <2267643286@qq.com>
2022-03-12 23:03:10 +0800

    hot1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("adfadfasdfasf");
+        adsfsdfsdf asdfasdfasdfasf;
     }
 
 }
