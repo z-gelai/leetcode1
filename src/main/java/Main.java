e1c6962017823d448b72eb25f7aea843dd74753f
z-gelai <94355846+z-gelai@users.noreply.github.com>
2022-03-12 22:55:34 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,9 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("adfadfasdfasf");
+    }
 
-    }
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> map=new HashMap<>();
-        for (String str : strs) {
-            char[] cs = str.toCharArray();
-            Arrays.sort(cs);
-            String s = String.valueOf(cs);
-            if (!map.containsKey(s)) {
-                map.put(s,new ArrayList<>());
-            }
-            map.get(s).add(str);
-        }
-        return new ArrayList<>(map.values());
-    }
 }
 
 
